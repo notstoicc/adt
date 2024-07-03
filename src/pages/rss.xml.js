bdc2e9880076542f74b96d3ae7f5751ff63bd97b
ノストイック <85674450+notstoicc@users.noreply.github.com>
2024-07-03 23:38:08 +0530

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,11 +2,11 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
 export async function get(context) {
-  const collection2022 = await getCollection('2022')
-  const collection2023 = await getCollection('2023')
-  const posts = [...collection2022, ...collection2023]
+  const collection2022 = await getCollection('2024')
+  const collection2023 = await getCollection('2025')
+  const posts = [...collection2024, ...collection2025]
   return rss({
-    title: 'yi',
+    title: 'ad',
     description: 'posts',
     site: context.site,
     items: posts.map(post => ({
